9c04b5ce4263a50f18f36c19f5a1158c79b1811e
AC-climoilou <1701633@cegeplimoilou.ca>
2023-11-07 16:13:59 -0500

    Continuation PlayerMovement

--- Jesus Funny Sunday/JFS/Assets/Scripts/Game/PlayerMovement.cs
+++ Jesus Funny Sunday/JFS/Assets/Scripts/Game/PlayerMovement.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float maxSpeed = 7f;
+    private Vector2 velocity;
+    private float maxSpeed = 10f;
     private float jumpHeight = 25f;
     private float playerGravity = 7f;
     private float moveDirection;
     private Rigidbody2D playerRB;
     private bool isOnGround;
-
+    private SpriteRenderer spriteRenderer;
+    public Sprite jesusIdle;
+    public Sprite jesusWalk1;
+    public Sprite jesusWalk2;
+    public Sprite jesusJump;
+    private float jesusWalkAnimationTimer = 0f;
+    private float animationTime = 0.1f;
+    
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
         playerRB.gravityScale = playerGravity;
-
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -27,10 +35,34 @@ public class PlayerMovement : MonoBehaviour
         //Facing angle
         if (moveDirection < 0)
         {
-            GetComponent<SpriteRenderer>().color = Color.green;
+            spriteRenderer.flipX = true;
+            
         } else if (moveDirection > 0)
         {
-            GetComponent<SpriteRenderer>().color = Color.yellow;
+            spriteRenderer.flipX = false;
+        }
+
+        if (moveDirection != 0 && isOnGround)
+        {
+            jesusWalkAnimationTimer += Time.deltaTime;
+
+            if (jesusWalkAnimationTimer >= animationTime)
+            {
+                jesusWalkAnimationTimer = 0.0f;
+
+                if (spriteRenderer.sprite == jesusWalk1)
+                {
+                    spriteRenderer.sprite = jesusWalk2;
+                }
+                else
+                {
+                    spriteRenderer.sprite = jesusWalk1;
+                }
+            }
+        }
+        else if (isOnGround)
+        {
+            spriteRenderer.sprite = jesusIdle;
         }
 
         //jump
@@ -38,13 +70,19 @@ public class PlayerMovement : MonoBehaviour
         {
             isOnGround = false;
             playerRB.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            spriteRenderer.sprite = jesusJump;
         }
     }
 
     void FixedUpdate()
     {
         //movement
-        Vector2 velocity = new Vector2(moveDirection * maxSpeed, playerRB.velocity.y);
+        if(isOnGround)
+        {
+            velocity = new Vector2(moveDirection * maxSpeed, playerRB.velocity.y);
+        }
+        else velocity = new Vector2(moveDirection * maxSpeed / 1.5f, playerRB.velocity.y);
+        
         playerRB.velocity = velocity;
     }
 
