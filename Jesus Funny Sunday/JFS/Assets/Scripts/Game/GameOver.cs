4254807c2b7490a4c3243c1bbf5880b79ca64277
AC-climoilou <1701633@cegeplimoilou.ca>
2023-11-24 08:00:49 -0500

    some sfx and music

--- Jesus Funny Sunday/JFS/Assets/Scripts/Game/GameOver.cs
+++ Jesus Funny Sunday/JFS/Assets/Scripts/Game/GameOver.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class GameOver : MonoBehaviour
 {
     public static GameOver instance;
+    public AudioClip gameOverSound;
+    private AudioSource cameraAudio;
+    
     private ParticleSystem blood;
+    public GameObject scream;
 
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
         blood = GameObject.Find("Blood Explosion").GetComponent<ParticleSystem>();
+
+        cameraAudio = GameObject.Find("Main Camera").GetComponent<AudioSource>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -23,6 +29,11 @@ public class GameOver : MonoBehaviour
             blood.transform.position = gameObject.transform.position;
 
             blood.Play();
+            
+            cameraAudio.clip = gameOverSound;
+            cameraAudio.Play();
+            
+            Instantiate(scream, transform.position, Quaternion.identity);
 
             Destroy(gameObject);
         }
