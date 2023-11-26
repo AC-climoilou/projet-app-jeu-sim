87e23fc035574a8792fdc71b87346541ee2424b7
Mathcegep <2135850@cegeplimoilou.ca>
2023-11-26 12:23:49 -0500

    attack delete gameobject

--- Jesus Funny Sunday/JFS/Assets/Scripts/Game/PlayerMovement.cs
+++ Jesus Funny Sunday/JFS/Assets/Scripts/Game/PlayerMovement.cs
@@ -158,10 +158,10 @@ public class PlayerMovement : MonoBehaviour
         //detect range
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
 
-        foreach(Collider2D enemy in hitEnemies)
+        foreach (Collider2D enemy in hitEnemies)
         {
-            //damage enemy
-            Debug.Log("Hitting " + enemy);
+            // Damage enemy
+            Destroy(enemy.gameObject);
         }
 
     }
